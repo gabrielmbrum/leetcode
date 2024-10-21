6b2ea1a14d5826c223d8c7f9e4064a4473a57736
Brum <gabrielbrum2805@gmail.com>
2024-10-21 12:51:07 -0300

    all sorted elements of 2 binary trees

--- allElementsOfTwoBinaryTree.c
+++ allElementsOfTwoBinaryTree.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdlib.h>
 
 #define MAX_NODES 5000
@@ -11,73 +10,57 @@ struct TreeNode {
 };
  
 
-void putInOrder (struct TreeNode* root, int index, int **arr) {
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+void putInOrder (struct TreeNode* root, int *index, int **arr) {
     if (root != NULL) {
-        putInOrder(root->left, index, &(*arr));
-        *arr[index++] = root->val;
-        putInOrder(root->right, index, &(*arr));
+        putInOrder(root->left, index, &(*arr));       // Traverse left subtree
+        (*arr)[(*index)++] = root->val;               // Store current node value
+        putInOrder(root->right, index, &(*arr));      // Traverse right subtree
     }
 }
 
-void merge(int **sorted, int *root1, int *root2) {
-    //index of each array
-    int r1, r2, s;
-    r1 = r2 = s = 0;
+void merge(int **sorted, int *root1, int root1Size, int *root2, int root2Size) {
+    // index of each array
+    int r1 = 0, r2 = 0, s = 0;
 
-    while (r1 < MAX_NODES && r2 < MAX_NODES) {
+    while (r1 < root1Size && r2 < root2Size) {
         if (root1[r1] < root2[r2]) 
-            *sorted[s++] = root1[r1++];
+            (*sorted)[s++] = root1[r1++];
         else
-            *sorted[s++] = root2[r2++];
+            (*sorted)[s++] = root2[r2++];
     }
 
-    // if already inserted all values of root1
-    if (r1 == MAX_NODES) {
-        while (r2 < MAX_NODES)
-            *sorted[s++] = root2[r2++];
-    } else {
-        while (r1 < MAX_NODES)
-            *sorted[s++] = root1[r1++];
-    }
+    // If any elements remain in root1
+    while (r1 < root1Size)
+        (*sorted)[s++] = root1[r1++];
+
+    // If any elements remain in root2
+    while (r2 < root2Size)
+        (*sorted)[s++] = root2[r2++];
 }
 
 int* getAllElements(struct TreeNode* root1, struct TreeNode* root2, int* returnSize) {
     int *sorted, *rootOne, *rootTwo;
-    sorted = (int*)malloc(sizeof(int) * MAX_NODES * 2);
+    int rootOneSize = 0, rootTwoSize = 0;
+    
     rootOne = (int*)malloc(sizeof(int) * MAX_NODES);
     rootTwo = (int*)malloc(sizeof(int) * MAX_NODES);
-    
-    putInOrder(root1, 0, &rootOne);
-    putInOrder(root2, 0, &rootTwo);
 
-    merge(&sorted, rootOne, rootTwo);
+    // Fill rootOne and rootTwo with the in-order traversal of each tree
+    putInOrder(root1, &rootOneSize, &rootOne);
+    putInOrder(root2, &rootTwoSize, &rootTwo);
+
+    *returnSize = rootOneSize + rootTwoSize;
+    sorted = (int*)malloc(sizeof(int) * (*returnSize));
+
+    // Merge the two sorted arrays
+    merge(&sorted, rootOne, rootOneSize, rootTwo, rootTwoSize);
+
+    // Free the temporary arrays
+    free(rootOne);
+    free(rootTwo);
 
     return sorted;
 }
-
-/*
-possível solução
-1 - percorrer in order e adicionar em 2 vetores distintos e depois agrupar eles
-
-2 - percorrer pelas duas ao mesmo tempo, comparando qual é menor (problema: n sei como)
-    -> 
-*/
-
-/*
-sol 1
-
-sol 2
-inOrder(struct TreeNode* root1, struct TreeNode* root2) {
-    if (root1 != null && root2 != null) {
-        if (root1->val < root2->val) {
-            inOrder(root1->left, root2);
-            add(arr1, root1->val);
-            inOrder(root1, root2->left)
-        } else {
-            inOder(root1, root2->left);
-            add(arr2, root2->val);
-        }
-    }
-}
-*/
-
